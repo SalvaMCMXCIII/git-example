836137ae96d84a0c838385b93d8cabf1b0f7f8c8
Dauren <daurenygmetov@gmail.com>
2022-04-22 12:06:15 +0600

    method sum has been added

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,12 @@
 public class Application {
     public static void main(String[] args) {
-        
+
+        int res = sum (7,8);
+        System.out.println(res);
+    }
+
+    private static int sum(int i, int i1) {
+        int sum = i + i1;
+        return sum;
     }
 }
