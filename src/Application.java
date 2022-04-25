9def68eae6967812ae4cdc35d90a81c730a443b1
Dauren <daurenygmetov@gmail.com>
2022-04-25 11:26:07 +0500

    method factorial has been added

--- src/Application.java
+++ src/Application.java
@@ -3,10 +3,19 @@ public class Application {
 
         int res = sum (7,8);
         System.out.println(res);
+        int factorial = factorial(5);
+        System.out.println(factorial);
     }
 
     private static int sum(int i, int i1) {
         int sum = i + i1;
         return sum;
+    }
+    private static int factorial(int a){
+        int fact = 1;
+        for(int i = 1; i <= a; i++){
+            fact = fact * i;
+        }
+        return fact;
     }
 }
