7f097ed58ce57025133f6f315c67931000c88e96
Dauren <daurenygmetov@gmail.com>
2022-04-29 10:51:35 +0500

    Method multiplication has been added

--- src/Application.java
+++ src/Application.java
@@ -7,6 +7,13 @@ public class Application {
         System.out.println(factorial);
         double res2 = sqr(2,2);
         System.out.println(res2);
+        int multiples = multiple(5,3);
+        System.out.println(multiples);
+    }
+
+    private static int multiple(int i, int i1){
+        int mult = i * i1;
+        return mult;
     }
 
     private static int sum(int i, int i1) {
