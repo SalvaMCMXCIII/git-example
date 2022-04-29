314f75b7a2fd838e586fa4874d925f17a69855b2
Dauren <salva@Daurens-MacBook-Air.local>
2022-04-29 10:35:36 +0500

    Square method has been added

--- src/Application.java
+++ src/Application.java
@@ -5,12 +5,19 @@ public class Application {
         System.out.println(res);
         int factorial = factorial(5);
         System.out.println(factorial);
+        double res2 = sqr(2,2);
+        System.out.println(res2);
     }
 
     private static int sum(int i, int i1) {
         int sum = i + i1;
         return sum;
     }
+    private static double sqr (int c, int d){
+       double sqr = Math.pow(c, d);
+       return sqr;
+    }
+
     private static int factorial(int a){
         int fact = 1;
         for(int i = 1; i <= a; i++){
